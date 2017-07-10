783ac0292e5913e11da727123230ac015fc18030
Frank Nasseri <franknasseri57@gmail.com>
2017-07-10 14:30:25 -0400

    added cat class

---
+++ IntrotoOOP/Cat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntrotoOOP
+{
+    class Cat
+    {
+        //Inside our class, we create our blueprint for our object 
+        //Classesare templates in OOP that we use to create objects.
+        //They should always be named in Pascal Case
+    }
+}
